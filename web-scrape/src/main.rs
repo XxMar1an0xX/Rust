7a10cfeeab1387f87a740a99760279acbdae7804
XxMar1an0xX <frodoepifanio@protonmail.com>
2026-01-16 16:09:45 -0300

    fetch de titulos y links funcional

--- web-scrape/src/main.rs
+++ web-scrape/src/main.rs
@@ -20,23 +20,24 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let seleccion_texto = Selector::parse(".text").unwrap();
 
     for bloque in html_fuente.select(&seleccion_quote) {
-        dbg!(bloque);
+        // dbg!(bloque);
         let frase = bloque
             .select(&seleccion_texto)
             .next()
             .map(|palabras| {
-                dbg!(palabras.text().collect::<String>());
+                // dbg!(palabras.text().collect::<String>());
                 palabras.text().collect::<Vec<_>>().join("")
             })
             .unwrap_or_default();
-        dbg!(frase);
+        // dbg!(frase);
     }
     let _ = olympus().await?;
     Ok(())
 }
 async fn olympus() -> Result<(), Box<dyn Error>> {
     println!("Olympus empieza...");
-    let link = "https://olympusbiblioteca.com/";
+    let mut link = "https://olympusbiblioteca.com/";
+    let mut link_agregado = "";
     let fuente_raw = reqwest::get(link).await?.text().await?;
 
     let html_fuente = Html::parse_document(&fuente_raw);
@@ -44,12 +45,24 @@ async fn olympus() -> Result<(), Box<dyn Error>> {
     let seleccion_manwha =
         Selector::parse("a.p-4,relative,bg-gray-800,rounded-md,overflow-hidden").unwrap();
     // Selector::parse(".flex,flex-col,gap-2,snap-start,shrink-0").unwrap();
-    let seleccion_titulo = Selector::parse(".font-medium text-lg h-13 line-clamp-2").unwrap();
+    let seleccion_texto = Selector::parse("h3.font-medium,text-lg,h-13,line-clamp-2").unwrap();
     // dbg!(&html_fuente);
-    println!("");
 
-    for bloque in html_fuente.select(&seleccion_manwha) {
-        dbg!(bloque);
+    for bloque_manwha in html_fuente.select(&seleccion_manwha) {
+        // dbg!(bloque_manwha.clone());
+        //NOTE: extraccion de link por medio de attributo de bloque
+        link_agregado = bloque_manwha.attr("href").unwrap_or_default();
+
+        let titulo = bloque_manwha
+            .select(&seleccion_texto)
+            //NOTE: seleccion del texto dentro del bloque
+            .next()
+            .map(|x| {
+                // dbg!(x);
+                x.text().collect::<String>()
+            })
+            .unwrap_or_default();
+        println!("{}: {}", titulo, link_agregado);
     }
 
     Ok(())
