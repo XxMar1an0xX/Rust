f1e98f993f8044c4ecb0257552ad942dacb9e5c7
XxMar1an0xX <frodoepifanio@protonmail.com>
2026-01-16 14:49:53 -0300

    logre hacer fetch de algo en olympus

--- web-scrape/src/main.rs
+++ web-scrape/src/main.rs
@@ -20,16 +20,37 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let seleccion_texto = Selector::parse(".text").unwrap();
 
     for bloque in html_fuente.select(&seleccion_quote) {
-        println!("== {:#?} ==\n", bloque);
+        dbg!(bloque);
         let frase = bloque
             .select(&seleccion_texto)
             .next()
             .map(|palabras| {
-                println!("> {:?} <\n", palabras.text().collect::<String>());
+                dbg!(palabras.text().collect::<String>());
                 palabras.text().collect::<Vec<_>>().join("")
             })
             .unwrap_or_default();
-        println!("\n {}", frase);
+        dbg!(frase);
     }
+    let _ = olympus().await?;
+    Ok(())
+}
+async fn olympus() -> Result<(), Box<dyn Error>> {
+    println!("Olympus empieza...");
+    let link = "https://olympusbiblioteca.com/";
+    let fuente_raw = reqwest::get(link).await?.text().await?;
+
+    let html_fuente = Html::parse_document(&fuente_raw);
+
+    let seleccion_manwha =
+        Selector::parse("a.p-4,relative,bg-gray-800,rounded-md,overflow-hidden").unwrap();
+    // Selector::parse(".flex,flex-col,gap-2,snap-start,shrink-0").unwrap();
+    let seleccion_titulo = Selector::parse(".font-medium text-lg h-13 line-clamp-2").unwrap();
+    // dbg!(&html_fuente);
+    println!("");
+
+    for bloque in html_fuente.select(&seleccion_manwha) {
+        dbg!(bloque);
+    }
+
     Ok(())
 }
