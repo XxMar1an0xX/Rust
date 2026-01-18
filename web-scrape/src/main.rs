bfbee6264d6e46d4597e3f47061b2b466155c388
XxMar1an0xX <frodoepifanio@protonmail.com>
2026-01-18 01:44:07 -0300

    añadido ir a otra pagina (frontera)

--- web-scrape/src/main.rs
+++ web-scrape/src/main.rs
@@ -31,12 +31,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
             .unwrap_or_default();
         // dbg!(frase);
     }
-    let _ = olympus().await?;
+    olympus().await?;
     Ok(())
 }
 async fn olympus() -> Result<(), Box<dyn Error>> {
     println!("Olympus empieza...");
-    let mut link = "https://olympusbiblioteca.com/";
+    let link = "https://olympusbiblioteca.com";
     let mut link_agregado = "";
     let fuente_raw = reqwest::get(link).await?.text().await?;
 
@@ -50,8 +50,6 @@ async fn olympus() -> Result<(), Box<dyn Error>> {
 
     for bloque_manwha in html_fuente.select(&seleccion_manwha) {
         // dbg!(bloque_manwha.clone());
-        //NOTE: extraccion de link por medio de attributo de bloque
-        link_agregado = bloque_manwha.attr("href").unwrap_or_default();
 
         let titulo = bloque_manwha
             .select(&seleccion_texto)
@@ -62,8 +60,36 @@ async fn olympus() -> Result<(), Box<dyn Error>> {
                 x.text().collect::<String>()
             })
             .unwrap_or_default();
+
+        //NOTE: extraccion de link por medio de attributo de bloque
+        if titulo == "Loco Frontera".to_string() {
+            link_agregado = bloque_manwha.attr("href").unwrap_or_default()
+        }
+
         println!("{}: {}", titulo, link_agregado);
     }
+    let link_lloyd = link.to_owned() + link_agregado;
+    dbg!(&link_lloyd);
+    frontera(link_lloyd).await?;
+
+    Ok(())
+}
+async fn frontera(link: String) -> Result<(), Box<dyn Error>> {
+    println!("Frontera Empieza BVVV");
+    let fuente_sin_formato = reqwest::get(link.as_str()).await?.text().await?;
+
+    let codigo_fuente = Html::parse_document(&fuente_sin_formato);
+
+    let seleccion_cap1 =
+        Selector::parse("a.text-amber-300,w-full,flex-between,p-3,rounded sf-ripple-container")
+            .unwrap();
+
+    let agregado_url = codigo_fuente
+        .select(&seleccion_cap1)
+        .map(|bloque| bloque.attr("href"))
+        .find(|&agregado| agregado.is_some())
+        .unwrap();
+    dbg!(agregado_url);
 
     Ok(())
 }
