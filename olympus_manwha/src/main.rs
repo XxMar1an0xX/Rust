dc8e1fc24a67fa4796629c5a14b3cdc85725da8a
XxMar1an0xX <frodoepifanio@protonmail.com>
2026-01-19 20:28:17 -0300

    No aparece el html del numero de capitulo olympus

--- olympus_manwha/src/main.rs
+++ olympus_manwha/src/main.rs
@@ -1,22 +1,26 @@
 use reqwest::blocking::{Response, get};
 use scraper::{Html, Selector};
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::copy;
 use std::{error::Error, fmt::Formatter};
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Olympus empieza...");
     let link = "https://olympusbiblioteca.com";
     let mut link_agregado = "";
+    //NOTE: aqui se "llama" al link para "ver" la pagina
     let fuente_raw = get(link)?.text()?;
 
+    //NOTE: aqui se lo formatea a HTML para que se pueda extraer ciertas partes o "bloques"
     let html_fuente = Html::parse_document(&fuente_raw);
 
+    //NOTE: aqui son los selectores, en base a la estructura del html
     let seleccion_manwha =
         Selector::parse("a.p-4,relative,bg-gray-800,rounded-md,overflow-hidden").unwrap();
     // Selector::parse(".flex,flex-col,gap-2,snap-start,shrink-0").unwrap();
     let seleccion_texto = Selector::parse("h3.font-medium,text-lg,h-13,line-clamp-2").unwrap();
     // dbg!(&html_fuente);
 
+    //NOTE: aqui es la iteracion de bloques flitradon en base a una seleccion
     for bloque_manwha in html_fuente.select(&seleccion_manwha) {
         // dbg!(bloque_manwha.clone());
 
@@ -65,7 +69,8 @@ fn frontera(link_base: &str, agregado_manwha: &str) -> Result<(), Box<dyn Error>
 
     let mut imagen = File::create("imagen.jpg")?;
     let mut response = extraer_img(link_base, agregado_url)?;
-    copy(&mut response, &mut imagen)?;
+    // copy(&mut response, &mut imagen)?; //NOTE: puede servir
+    fs::write("hola.webp", response.bytes()?);
     // extraer_img(link_base, agregado_url)?;
     Ok(())
 }
@@ -73,12 +78,41 @@ fn extraer_img(link_base: &str, agregado_cap: &str) -> Result<Response, Box<dyn 
     let link_cap = link_base.to_string() + agregado_cap;
     let respuesta = get(link_cap.as_str())?.text()?;
     let codigo_fuente = Html::parse_document(&respuesta);
+    let seleccion_manwha =
+        Selector::parse(".flex-col,rounded-xl,overflow-hidden,shadow-xl").unwrap();
     let seleccion_img = Selector::parse("img.object-cover,rounded-inherit,w-full,h-full").unwrap();
+    // let seleccion_titulo = Selector::parse("div.flex-between,w-full").unwrap();
+    let seleccion_titulo = Selector::parse("header.h-18,bg-gray-800,relative,z-20").unwrap();
+    let seleccion_cap_num = Selector::parse(".flex-center").unwrap();
+
+    let seccion_titulo = codigo_fuente.select(&seleccion_titulo).next().unwrap();
+    dbg!(&seccion_titulo.html());
+
+    let nombre_carpeta = seccion_titulo
+        .select(&seleccion_cap_num)
+        .next()
+        .map(|bloque| {
+            dbg!(&bloque);
+            bloque.text().collect::<String>()
+        })
+        .unwrap_or_default();
+    dbg!(&nombre_carpeta);
+
+    let manwha_completo = codigo_fuente.select(&seleccion_manwha).next().unwrap();
+    // dbg!(&manwha_completo);
+
+    for link in manwha_completo
+        .select(&seleccion_img)
+        .filter_map(|bloque| bloque.attr("src"))
+    {
+        let nombre_imagen = &link[(74 - 9)..];
+        // dbg!(nombre_imagen);
+    }
 
     let link_img = codigo_fuente
         .select(&seleccion_img)
         .filter_map(|bloque| bloque.attr("src"))
-        .nth(1)
+        .nth(4)
         .unwrap();
 
     dbg!(&link_img);
